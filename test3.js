fdff682d17b72029807d47314d9053c6554b8ee7
Ali Hamdan <ali.hamdan.lb05@gmail.com>
2026-08-03 16:27:49 +0300

    Create test3.js

---
+++ test3.js
@@ -0,0 +1,31 @@
+"use strict";
+//Property modifiers Exercise
+var Shape;
+(function (Shape) {
+    Shape[Shape["Circle"] = 0] = "Circle";
+    Shape[Shape["Rectangle"] = 1] = "Rectangle";
+    Shape[Shape["Triangle"] = 2] = "Triangle";
+})(Shape || (Shape = {}));
+function draw({ shape, xpos = 0, ypos = 0 }) {
+    switch (shape) {
+        case Shape.Circle:
+            console.log(`Drawing circle at (${xpos}, ${ypos})`);
+            break;
+        case Shape.Rectangle:
+            console.log(`Drawing rectangle at (${xpos}, ${ypos})`);
+            break;
+        case Shape.Triangle:
+            console.log(`Drawing triangle at (${xpos}, ${ypos})`);
+            break;
+    }
+}
+draw({ shape: Shape.Circle });
+draw({ shape: Shape.Rectangle, xpos: 10, ypos: 20 });
+const grades = {
+    Ali: 85,
+    Jawad: 2,
+    Roaa: 95
+};
+for (let student in grades) {
+    console.log(`${student}: ${grades[student]}`);
+}
