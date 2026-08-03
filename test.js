a3a15c3dfbb99bb89f609fca4b3ea1656005f0c3
Ali Hamdan <ali.hamdan.lb05@gmail.com>
2026-08-03 12:11:28 +0300

    Practice Union Types and Type Alias

--- test.js
+++ test.js
@@ -1,2 +1,18 @@
 "use strict";
 console.log("Hello, World!");
+let person = "Ali";
+let date = new Date();
+greet(person, date);
+function greet(person, date) {
+    console.log(`Hello, ${person}, Today is ${date.toDateString()}.`);
+}
+const var1 = () => {
+    return person + date;
+};
+let me = {
+    name: person,
+    age: 21
+};
+const obj = {
+    string1: var1()
+};
--- test.ts
+++ test.ts
@@ -1,1 +1,31 @@
 console.log("Hello, World!");
+let person = "Ali";
+let date : Date = new Date();
+
+greet(person, date);
+
+function greet(person: string, date: Date){
+    console.log(`Hello, ${person}, Today is ${date.toDateString()}.`);
+}
+
+//Union Types Example
+const var1: string | number | Function = () => {
+    return person + date;
+} 
+
+//Type Alias Example
+type Person = {
+    name: string,
+    age: number
+}
+let me: Person = {
+    name: person,
+    age: 21
+}
+
+//Combine Type Alias and object creation
+const obj: {
+    string1 : string
+} = {
+    string1: var1()
+}
